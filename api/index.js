0aad0c67a54f04f21fe2e8cab71b668b40c7e258
lauti3314 <lautaro3314@hotmail.com>
2023-02-14 13:20:16 -0300

    re

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const port = process.env.DB_PORT || 3001;
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
