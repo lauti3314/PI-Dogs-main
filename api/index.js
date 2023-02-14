09c4a0e1ec4f99fb24de65e48ea13b3ed14b3221
lauti3314 <lautaro3314@hotmail.com>
2023-02-14 13:09:07 -0300

    a

--- api/index.js
+++ api/index.js
@@ -17,9 +17,11 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001;
+const port = process.env.DB_PORT || 3001;
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
